e2e20c86e9593fef1cb5d418d80030ff6f064bf7
Kyra Christensen <christensen.kyral@gmail.com>
2022-05-14 06:33:33 -0700

    all tests pass

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -25,5 +25,51 @@ describe('App', () => {
     expect(listItem).toHaveTextContent('tomato', { exact: false });
 });
 
+it('should delete an item on delete button click', async () => {
+    const addInput = screen.getByPlaceholderText('New Item');
+    userEvent.type(addInput, 'apples');
 
+    const addButton = await screen.findByLabelText('add item');
+
+    userEvent.click(addButton);
+    
+    const listItem = await screen.findByRole('list-item');
+
+    const deleteButton = await screen.findByLabelText(`delete apples`);
+
+    userEvent.click(deleteButton);
+    
+    expect(listItem).not.toBeInTheDocument();
+});
+
+it('should edit a list item', async () => {
+    const addInput = screen.getByPlaceholderText('New Item');
+    userEvent.type(addInput, 'apples');
+    const addButton = await screen.findByLabelText('add item');
+    userEvent.click(addButton);
+
+    const editButton = await screen.findByLabelText('edit apples');
+    userEvent.click(editButton);
+    const editInput = await screen.findByLabelText('edit input');
+    userEvent.type(editInput, 'apples!!!!!!');
+    const saveButton = await screen.findByLabelText('save edits');
+
+    const listItem = await screen.findByRole('list-item');
+    expect(listItem).toHaveTextContent('apples!!!!!!');
+});
+
+it('should display a list of items', async () => {
+    const addInput = screen.getByPlaceholderText('New Item');
+    userEvent.type(addInput, 'apples');
+    const addButton = await screen.findByLabelText('add item');
+    userEvent.click(addButton);
+
+    userEvent.type(addInput, 'bananas');
+    userEvent.click(addButton);
+
+
+    const listItems = await screen.findAllByRole('list-item');
+
+    expect(listItems.length).toBe(2);
+});
 })
