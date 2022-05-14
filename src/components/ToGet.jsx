c8aaf3b1d2fe207566b8b5ed4b8d5253dd83a96f
Kyra Christensen <christensen.kyral@gmail.com>
2022-05-14 05:08:46 -0700

    completed toGet component

--- src/components/ToGet.jsx
+++ src/components/ToGet.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function ToGet() {
+  const [isEditing, setIsEditing] = useState(false);
+
+  let content;
+
+  if (isEditing) {
+    content = (
+      <form onSubmit={(event) => { event.preventDefault(); setIsEditing(false); }}>
+        <input aria-label='edit input' value={toget.text} onChange={(e) => onUpdate({ ...toget, text: e.target.value })}></input>
+        <button aria-label='save edits'>Save</button>
+      </form>
+    ) 
+  } else {
+    content = (
+      <button aria-label='edit button' type='button' onClick={() => setIsEditing(true)}>Edit</button>
+    )
+  }
+
   return (
-    <div>ToGet</div>
+    <>
+      <input type='checkbox' checked={toget.done} onChange={(e) => {
+        onUpdate({ ...toget, done: e.target.checked });
+      }}/>
+      {toget.text}
+      {content}
+      <button aria-label='delete button' type='button' onClick={() => onDelete(toget.id)}>Delete Item</button>
+    </>
   )
 }
