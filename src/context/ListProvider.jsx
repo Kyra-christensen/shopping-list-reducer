f8ca9678c261ca8fe20c16bc842b03fa0df26194
Kyra Christensen <christensen.kyral@gmail.com>
2022-05-14 03:46:22 -0700

    added reducer to context file

--- src/context/ListProvider.jsx
+++ src/context/ListProvider.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { createContext, useContext, useReducer } from 'react';
 
-export default function ListProvider() {
-  return (
-    <div>ListProvider</div>
-  )
-}
+const initialToGets = [{ id: Date.now(), text: 'apples', done: false }];
+
+const toGetReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TOGET':
+      return [ ...state, { id: Date.now(), text: action.payload.text, done: false }];
+    case 'UPDATE_TOGET':
+      return state.map((toget) => {
+        if(toget.id === action.payload.toget.id) {
+          const { done, text } = action.payload.toget;
+          return { ...toget, text, done };
+        }
+        return toget;
+      });
+    case 'DELETE_TOGET':
+      return state.filter((toget) => toget.id !== action.payload.id);
+    default:
+      throw new Error(`Action type ${action.type} is not supported.`);
+  }
+};
