66b73efacfedc1f378cc79d663ff0a0d7468e88a
Kyra Christensen <christensen.kyral@gmail.com>
2022-05-14 04:57:45 -0700

    completed header.jsx file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { useToGets } from '../context/ListProvider';
 
 export default function Header() {
+  const { togets, handleDeleteAllToGets } = useToGets();
+
   return (
-    <div>Header</div>
+    <>
+      <div>Number of Items: {togets.length}</div>
+      <button onClick={handleDeleteAllToGets}>Clear List</button>
+    </>
   )
 }
